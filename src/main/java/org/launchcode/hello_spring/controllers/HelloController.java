2e742e56ee08f468044658b6bc3f6030397673a3
Tiffany <139176913+talph101@users.noreply.github.com>
2024-10-22 12:04:32 -0500

    Completed controller exercise. Gave drop down menu to form with different languages for greeting.

--- src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -58,9 +58,47 @@ public class HelloController {
 //                "</html>";
 //    }
 
-    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
-    public String holaWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+//    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
+//    public String holaWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
+
+//    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
+//    public String holaWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+
+
+    //}
+
+    @RequestMapping(value ="/hello", method = RequestMethod.POST)
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
     }
 
     @GetMapping("/form")
@@ -69,6 +107,13 @@ public class HelloController {
                 "<body>" +
                 "<form action='hello' method='post'>" + //submit a request to /hola
                 "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                "<option value= 'english'> English </option>" +
+                "<option value= 'french'> French </option>" +
+                "<option value= 'german'> German </option>" +
+                "<option value= 'spanish'> Spanish </option>" +
+                "<option value= 'japanese'> Italian </option>" +
+                "</select>" +
                 "<input type='submit' value='Greet me!'> " +
                 "</form>" +
                 "</body>" +
